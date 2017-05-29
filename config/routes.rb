31569054b014b9e9e80ccc2c8fd61dd7d80f5faa
janl <jan-luetzen@hotmail.de>
2017-05-29 15:46:26 +0000

    Modify readme and replaced Welcome controller with Page controller, and set new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  get 'welcome/home'
+ root 'page#home'
+
+ get 'about', to: 'page#about'
+
+ 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
 
- root 'welcome#home'
- get 'about', to: 'welcome#about'
+
  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
